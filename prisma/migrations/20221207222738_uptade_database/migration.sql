452835d7962f87ba61c58da4754788e0128fba02
Larissa Fernanda <larifernand7@gmail.com>
2022-12-15 23:45:51 -0300

    att

--- prisma/migrations/20221207222738_uptade_database/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "AttendanceList" DROP CONSTRAINT "AttendanceList_classroomId_fkey";
-
--- AddForeignKey
-ALTER TABLE "AttendanceList" ADD CONSTRAINT "AttendanceList_classroomId_fkey" FOREIGN KEY ("classroomId") REFERENCES "Classroom"("id") ON DELETE CASCADE ON UPDATE CASCADE;
